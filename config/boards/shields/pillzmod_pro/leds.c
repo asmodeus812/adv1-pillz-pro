00795a5c96982df25c164b1a9128c5660c9a9b2e
Svetlozar Iliev <svetlozar321@gmail.com>
2024-12-22 19:26:52 +0200

    feat(led): replace scroll lock led indicator for a layer indicator

--- config/boards/shields/pillzmod_pro/leds.c
+++ config/boards/shields/pillzmod_pro/leds.c
@@ -1,21 +1,23 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/led.h>
-#include <zephyr/kernel.h>
 #include <zephyr/init.h>
-#include <zmk/hid_indicators.h>
+#include <zephyr/kernel.h>
 #include <zmk/events/hid_indicators_changed.h>
+#include <zmk/events/layer_state_changed.h>
+#include <zmk/hid_indicators.h>
 
 #define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
 
 // GPIO-based LED device
 static const struct device *led_dev = DEVICE_DT_GET(LED_GPIO_NODE_ID);
 
-static int led_keylock_listener_cb(const zmk_event_t *eh) {
+static int led_indication_listener_cb(const zmk_event_t *eh) {
+    const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
     zmk_hid_indicators_t flags = zmk_hid_indicators_get_current_profile();
     unsigned int capsBit = 1 << (HID_USAGE_LED_CAPS_LOCK - 1);
     unsigned int numBit = 1 << (HID_USAGE_LED_NUM_LOCK - 1);
-    unsigned int scrollBit = 1 << (HID_USAGE_LED_SCROLL_LOCK - 1);
+    // unsigned int scrollBit = 1 << (HID_USAGE_LED_SCROLL_LOCK - 1);
 
     if (flags & capsBit) {
         led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
@@ -29,7 +31,13 @@ static int led_keylock_listener_cb(const zmk_event_t *eh) {
         led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_num)));
     }
 
-    if (flags & scrollBit) {
+    // if (flags & scrollBit) {
+    //     led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_scroll)));
+    // } else {
+    //     led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_scroll)));
+    // }
+
+    if (ev->layer != 0 && ev->state) {
         led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_scroll)));
     } else {
         led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_scroll)));
@@ -38,7 +46,8 @@ static int led_keylock_listener_cb(const zmk_event_t *eh) {
     return 0;
 }
 
-ZMK_LISTENER(led_indicators_listener, led_keylock_listener_cb);
+ZMK_LISTENER(led_indicators_listener, led_indication_listener_cb);
+ZMK_SUBSCRIPTION(led_indicators_listener, zmk_layer_state_changed);
 ZMK_SUBSCRIPTION(led_indicators_listener, zmk_hid_indicators_changed);
 
 static int leds_init(const struct device *device) {
